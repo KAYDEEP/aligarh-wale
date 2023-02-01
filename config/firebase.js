47212c32614e66d6532d541c8e5356e51b29286f
kuldeep404 <kuldeep.kumar@cardekho.com>
2023-02-01 14:25:57 +0530

    config updated

--- config/firebase.js
+++ config/firebase.js
@@ -3,19 +3,15 @@ import { initializeApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC307_IX1KgxVvF9crXPL_dSeoWyQAdtfE",
-  authDomain: "aligarh-f1797.firebaseapp.com",
-  projectId: "aligarh-f1797",
-  storageBucket: "aligarh-f1797.appspot.com",
-  messagingSenderId: "338899544341",
-  appId: "1:338899544341:web:446ab02250edf7e9a1e11c",
-  measurementId: "G-8F3BC9VT2V"
+  apiKey: process.env.FB_API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.FB_APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
 };
 
 // Initialize Firebase
